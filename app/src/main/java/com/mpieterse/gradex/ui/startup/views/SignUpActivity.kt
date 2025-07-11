75d4be6b8f623288e6c9f3bffbeefe7e71b2b77d
mattpieterse <63480247+mattpieterse@users.noreply.github.com>
2025-07-11 02:16:46 +0200

    feat(ui): create and register sign-up activity viewmodel

--- app/src/main/java/com/mpieterse/gradex/ui/startup/views/SignUpActivity.kt
+++ app/src/main/java/com/mpieterse/gradex/ui/startup/views/SignUpActivity.kt
@@ -5,14 +5,57 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.mpieterse.gradex.R
+import androidx.lifecycle.ViewModelProvider
+import com.mpieterse.gradex.core.utils.Clogger
+import com.mpieterse.gradex.databinding.ActivitySignUpBinding
+import com.mpieterse.gradex.ui.startup.viewmodels.SignUpViewModel
 
 class SignUpActivity : AppCompatActivity() {
+    companion object {
+        private const val TAG = "SignUpActivity"
+    }
+
+
+    private lateinit var binds: ActivitySignUpBinding
+    private lateinit var model: SignUpViewModel
+
+
+    // --- Lifecycle
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Clogger.d(
+            TAG, "Created a new instance of the activity"
+        )
+
+        setupBindings()
+        setupLayoutUi()
+
+        model = ViewModelProvider(this)[SignUpViewModel::class.java]
+
+        observe()
+    }
+
+
+    // --- ViewModel
+
+
+    private fun observe() {}
+
+
+    // --- UI
+
+
+    private fun setupBindings() {
+        binds = ActivitySignUpBinding.inflate(layoutInflater)
+    }
+
+
+    private fun setupLayoutUi() {
+        setContentView(binds.root)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_sign_up)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(binds.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
