827fbaa4008a5d04851f6e658bff3d0829e3ae69
mattpieterse <63480247+mattpieterse@users.noreply.github.com>
2025-07-11 15:51:13 +0200

    build(gradle/deps): add dependency for androidx lifecycle viewmodel

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation(libs.androidx.core.splashscreen)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
 
     // Firebase
 
