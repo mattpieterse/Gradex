64d5744edb31fe024f54c542bc5a0aeed7f62989
mattpieterse <63480247+mattpieterse@users.noreply.github.com>
2025-07-16 21:31:25 +0200

    feat(ui): create home-overview-fragment with view-binding

---
+++ app/src/main/java/com/mpieterse/gradex/ui/central/views/HomeOverviewFragment.kt
@@ -0,0 +1,43 @@
+package com.mpieterse.gradex.ui.central.views
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.mpieterse.gradex.databinding.FragmentHomeOverviewBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HomeOverviewFragment : Fragment() {
+    companion object {
+        private const val TAG = "HomeOverviewFragment"
+    }
+
+
+    private lateinit var binds: FragmentHomeOverviewBinding
+
+
+// --- Lifecycle
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? = createBindings(inflater, container)
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+
+// --- UI
+
+
+    private fun createBindings(
+        inflater: LayoutInflater, container: ViewGroup?
+    ): View {
+        binds = FragmentHomeOverviewBinding.inflate(inflater, container, false)
+        return binds.root
+    }
+}
