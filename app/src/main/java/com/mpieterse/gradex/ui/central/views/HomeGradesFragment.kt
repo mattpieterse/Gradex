bc378cd814c05fe56954d42b7fd25c74e29ba666
mattpieterse <63480247+mattpieterse@users.noreply.github.com>
2025-07-16 21:30:46 +0200

    feat(ui): create home-grades-fragment with view-binding

---
+++ app/src/main/java/com/mpieterse/gradex/ui/central/views/HomeGradesFragment.kt
@@ -0,0 +1,43 @@
+package com.mpieterse.gradex.ui.central.views
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.mpieterse.gradex.databinding.FragmentHomeGradesBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HomeGradesFragment : Fragment() {
+    companion object {
+        private const val TAG = "HomeGradesFragment"
+    }
+
+
+    private lateinit var binds: FragmentHomeGradesBinding
+
+
+// --- Lifecycle
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? = createBindings(inflater, container)
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+
+// --- UI
+
+
+    private fun createBindings(
+        inflater: LayoutInflater, container: ViewGroup?
+    ): View {
+        binds = FragmentHomeGradesBinding.inflate(inflater, container, false)
+        return binds.root
+    }
+}
