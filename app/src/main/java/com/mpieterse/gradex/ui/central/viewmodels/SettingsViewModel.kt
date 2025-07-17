532864c666b887110ef5c612418d58cbe4b4152c
mattpieterse <63480247+mattpieterse@users.noreply.github.com>
2025-07-17 22:38:29 +0200

    fix(ui/act-settings): add missing inject constructor in viewmodel

--- app/src/main/java/com/mpieterse/gradex/ui/central/viewmodels/SettingsViewModel.kt
+++ app/src/main/java/com/mpieterse/gradex/ui/central/viewmodels/SettingsViewModel.kt
@@ -2,9 +2,10 @@ package com.mpieterse.gradex.ui.central.viewmodels
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class SettingsViewModel: ViewModel() {
+class SettingsViewModel @Inject constructor(): ViewModel() {
     companion object {
         private const val TAG = "SettingsViewModel"
     }
